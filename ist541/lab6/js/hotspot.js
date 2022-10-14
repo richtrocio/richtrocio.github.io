4ac1cb162fc0773f08ae697680acbc387b295ddb
richtrocio <113489881+richtrocio@users.noreply.github.com>
2022-10-13 23:05:10 -0500

    color update

--- ist541/lab6/js/hotspot.js
+++ ist541/lab6/js/hotspot.js
@@ -3,7 +3,7 @@ $( function() {
    $("map area").click( function(){
 
       if ($(this).attr("id") == "College of Engineering Building") {
-         $("#feedback").css("color","red");
+         $("#feedback").css("color","green");
          $("#feedback").html("Yes, faculty and academic advisors are located in the College of Engineering building");
       }
 
@@ -13,12 +13,12 @@ $( function() {
       }
 
       if ($(this).attr("id") == "Student Wellness Center") {
-         $("#feedback").css("color","red");
+         $("#feedback").css("color","green");
          $("#feedback").html("Yes, you may find the health center and other wellness support at the Student Wellness Center");
       }
 	   
 	   if ($(this).attr("id") == "Student Recreation and Aquatic Center") {
-         $("#feedback").css("color","red");
+         $("#feedback").css("color","green");
          $("#feedback").html("Yes, you may find the gym and other wellness services at the Student Recreation and Aquatic Center");
       }
 
